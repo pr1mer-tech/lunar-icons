9be7d163c755a1ad2b2bad7510387eadd4f9d540
lucasgruwez <gruwez.lucas@gmail.com>
2017-06-05 17:23:16 +1000

    :wheelchair:

--- docs/_site/js/plugins.js
+++
@@ -1,3 +0,0 @@
-function toggleMenu() {
-  $('nav ul').toggleClass('visible');
-}
---
+++ docs/js/nav-menu.js
@@ -0,0 +1,69 @@
+class navMenu {
+  constructor() {
+    this.linkEl   = document.querySelectorAll('nav ul li a');
+    this.toggleEl = document.querySelector('#menuToggle')
+    this.menuEl   = document.querySelector('nav ul');
+    this.headerEl = document.querySelector('header');
+    this.footerEl = document.querySelector('footer');
+    this.mainEl   = document.querySelector('main');
+
+    this.viewportW    = window.innerWidth;
+    this.isVisible    = false;
+
+    this.showMenu     = this.showMenu.bind(this);
+    this.hideMenu     = this.hideMenu.bind(this);
+    this.toggleMenu   = this.toggleMenu.bind(this);
+    this.handleResize = this.handleResize.bind(this);
+
+    this._addEventListeners();
+    this.handleResize();
+  }
+
+  _addEventListeners() {
+    for (var i = 0; i < this.linkEl.length; i++) {
+      this.linkEl[i].addEventListener('click', this.hideMenu);
+    }
+    this.toggleEl.addEventListener('click', this.toggleMenu);
+    window.addEventListener('resize', this.handleResize)
+  }
+
+  _contentInert(bool) {
+    this.headerEl.inert = bool;
+    this.footerEl.inert = bool;
+    this.mainEl.inert   = bool;
+  }
+
+  handleResize() {
+    this.viewportW = window.innerWidth;
+
+    if (this.viewportW > 840) {
+      this.showMenu();
+      this._contentInert(false);
+    } else {
+      this.menuEl.classList.remove('animate')
+      this.hideMenu();
+    }
+  }
+
+  showMenu() {
+    this.menuEl.classList.add('animate');
+    this.menuEl.classList.add('visible');
+    this.menuEl.inert   = false;
+    this.isVisible      = true;
+    this._contentInert(true);
+  }
+
+  hideMenu() {
+    this.menuEl.classList.remove('visible');
+    this.menuEl.inert   = true;
+    this.isVisible      = false;
+    this._contentInert(false);
+  }
+
+  toggleMenu() {
+    this.isVisible ? this.hideMenu() : this.showMenu();
+  }
+}
+
+
+new navMenu();
