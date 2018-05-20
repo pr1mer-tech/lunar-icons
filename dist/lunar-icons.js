9c69d74b3fe1aa535e2da0ac8c6c98c9d5c6ddfb
lucasgruwez <gruwez.lucas@gmail.com>
2018-05-20 21:16:18 +1000

    :building_construction: Build gulp

--- dist/lunar-icons.js
+++ dist/lunar-icons.js
@@ -1,3 +1,10 @@
+/*!
+ * lunar-icons - 2.1.0 - (https://lucasgruwez.github.io/lunar-icons)
+ * Copyright 2018 Lucas Gruwez.
+ * Licensed under MIT
+ * https://github.com/lucasgruwez/lunar-icons
+ */
+
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 module.exports={
     "alarm": "<circle cx=\"12\" cy=\"14\" r=\"8\"/><path d=\"M7.79 3.84a11 11 0 0 0-4.94 4.05m13.36-4.05a11 11 0 0 1 4.94 4.05M12 9v5l3 3\"/>",
