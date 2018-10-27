7bc96a935999f49cbcf442e8eacbac0b775b8c69
Arthur Guiot <arguiot@gmail.com>
2018-10-27 20:58:29 +0800

    Fix file read error in gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -49,7 +49,7 @@ gulp.task("CSS", () => {
 	let css = ""
 	fs.readdir('./src/icons/', (err, files) => {
 		files.forEach(name => {
-			const svg = fs.readFileSync(name).toString().replace(/(\r\n\t|\n|\r\t)/gm,"") // remove line breaks
+			const svg = fs.readFileSync(`./src/icons/${name}`).toString().replace(/(\r\n\t|\n|\r\t)/gm,"") // remove line breaks
 			const icon = path.basename(name, '.svg') // removes extension
 			css += `lunar-icon[icon="${icon}"] {content: url(data:image/svg+xml;charset=utf8,${encodeURIComponent(svg)});}`
 		})
